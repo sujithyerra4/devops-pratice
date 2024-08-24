86988bd1914718c22710ce5c70e93b758a5fda48
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-24 12:13:53 +0530

    chnaged

--- 03-varaibles.sh
+++ 03-varaibles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERSON1=A
 PERSON2=B
-echo "$A:hey"
-echo "$B:hello"
-echo "$A:hw r u $B"
-echo "$B: fine $A"
+echo "$A::hey"
+echo "$B::hello"
+echo "$A::hw r u $B"
+echo "$B:: fine $A"
