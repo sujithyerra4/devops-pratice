62b6e6567e9afe03ee75f44c1d4bbef50289babf
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-24 12:37:16 +0530

    runtime

---
+++ 06-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("APPLLE" "ORANGE" "GRAPES")
+
+echo "1st fruit is :$FRUITS[0]"
+echo "2st fruit is :$FRUITS[1]"
+echo "3st fruit is :$FRUITS[2]"
+echo "all fruit is :$FRUITS[@]"
