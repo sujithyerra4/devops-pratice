1a7a2e349c1bef3f192e1893d1888f3b065869ec
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-24 12:39:36 +0530

    runtime

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,7 +2,7 @@
 
 FRUITS=("APPLLE" "ORANGE" "GRAPES")
 
-echo "1st fruit is :$FRUITS[0]"
-echo "2st fruit is :$FRUITS[1]"
-echo "3st fruit is :$FRUITS[2]"
-echo "all fruit is :$FRUITS[@]"
+echo "1st fruit is :${FRUITS[0]}"
+echo "2st fruit is :${FRUITS[1]}"
+echo "3st fruit is :${FRUITS[2]}"
+echo "all fruit is :${FRUITS[@]}"
