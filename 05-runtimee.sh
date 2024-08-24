9ec5592566f5eca82191924753530c9a32d7a7fe
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-24 12:33:28 +0530

    runtime

--- 05-runtimee.sh
+++ 05-runtimee.sh
@@ -3,10 +3,10 @@
 echo "please enter usrname:"
 read -s usernam
 
-echo "username enter is : $username"
+echo "username enter is : $usernam"
 
 echo "please enter pass:"
 read -s usernam
 
-echo "username enter is : $username"
+echo "username enter is : $usernam"
 
